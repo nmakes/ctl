6771b1149e371f0ae09a5e33d1bfc3c877f13728
nmakes <nav.naveenvenkat@gmail.com>
2020-08-15 16:09:42 +0530

    adding transposed search

--- ctlib/ctemplate_searching.c
+++ ctlib/ctemplate_searching.c
@@ -55,6 +55,24 @@ ctl_size_t ctl_linear_search(
 }
 
 
+ctl_size_t ctl_linear_search_transpose( // Performs transposition after searching
+	ctemplate arr, // ctemplate containing a pointer to the starting address of the sorted array (and size)
+	ctl_size_t length, // Length of the array
+	ctemplate key, // ctemplate containing pointer to the value to be searched for
+	int (*compare)(ctemplate, ctemplate), // Pointer to the comparator function
+	void (*assign)(ctemplate, ctemplate)) // Pointer to the assign function (for deepswap)
+{
+	ctl_size_t index = ctl_linear_search(arr, length, key, compare);
+
+	if ( (index < length) && (index > 0))
+	{
+		ctl_deep_swap( ctl_next(arr, index), ctl_next(arr, index-1), assign );
+	}
+
+	return index
+}
+
+
 ctemplate ctl_max(
 	ctemplate arr, // ctemplate containing a pointer to the starting address of the array (and element size)
 	ctl_size_t length, // Length of the array
