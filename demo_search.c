c6717eb3dfd8285a478640ce113794a1160b6479
nmakes <nav.naveenvenkat@gmail.com>
2020-08-15 16:20:04 +0530

    adding search with transposition demo

--- demo_search.c
+++ demo_search.c
@@ -33,6 +33,12 @@ int demo_compare(ctemplate a, ctemplate b)
 }
 
 
+void demo_assign(ctemplate a, ctemplate b)
+{
+	* (int*) a.ptr = * (int*) b.ptr;
+}
+
+
 void demo_binary_search(int * a, int N, int key)
 {
 	// Create a ctemplate for a - to be used by the library
@@ -75,19 +81,76 @@ void demo_linear_search(int * a, int N, int key)
 }
 
 
+void demo_linear_search_transpose(int * a, int N, int key)
+{
+	// Create a ctemplate for a - to be used by the library
+	ctemplate 		t_a = {a, sizeof(int)};
+	ctl_size_t 		t_N = N;
+	ctemplate 		t_key = {&key, sizeof(int)};
+
+	// Run the binary search algorithm
+	ctl_size_t 		pos = ctl_linear_search_transpose(t_a, t_N, t_key, &demo_compare, &demo_assign);
+
+	if(pos == N)
+	{
+		printf("Number %d not found\n", key);
+	}
+	else
+	{
+		printf("Found number %d at position: %ld\n", key, pos);
+	}
+}
+
+
+void demo_linear_search_transpose_exponential(int * a, int N, int key)
+{
+	// Create a ctemplate for a - to be used by the library
+	ctemplate 		t_a = {a, sizeof(int)};
+	ctl_size_t 		t_N = N;
+	ctemplate 		t_key = {&key, sizeof(int)};
+
+	// Run the binary search algorithm
+	ctl_size_t 		pos = ctl_linear_search_transpose_exponential(t_a, t_N, t_key, &demo_compare, &demo_assign);
+
+	if(pos == N)
+	{
+		printf("Number %d not found\n", key);
+	}
+	else
+	{
+		printf("Found number %d at position: %ld\n", key, pos);
+	}
+}
+
+void plot(int * a, int N)
+{
+	printf("Array Is: ");
+
+	for(int i=0; i<N; i++)
+	{
+		printf("%d ", a[i]);
+	}
+
+	printf("\n\n");
+}
+
 int main()
 {
 	// Create an array
 	int a[] = {1, 6, 7, 15, 18, 24, 39, 45, 50, 62, 88, 100};
 	int N = sizeof(a) / sizeof(int);
 
-	demo_binary_search(a, N, 39);
+	plot(a, N);
 
-	demo_binary_search(a, N, 25);
+	demo_binary_search(a, N, 39); plot(a, N);
 
-	demo_linear_search(a, N, 1);
+	demo_binary_search(a, N, 25); plot(a, N);
 
-	demo_linear_search(a, N, 19);
+	demo_linear_search(a, N, 88); plot(a, N);
+
+	demo_linear_search_transpose(a, N, 18); plot(a, N);
+
+	demo_linear_search_transpose_exponential(a, N, 24); plot(a, N);
 
 
 }
